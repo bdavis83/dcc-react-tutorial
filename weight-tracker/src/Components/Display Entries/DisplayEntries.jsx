d0d9a69020a39908aee78ab42bd47542f61ff452
bdavis83 <bdavis83@gmail.com>
2023-02-19 17:53:03 -0500

    Created Display entries component

---
+++ weight-tracker/src/Components/Display Entries/DisplayEntries.jsx
@@ -0,0 +1,27 @@
+
+const DisplayEntries = (props) => {
+    return ( 
+        <table>
+        <thead>
+          <tr>
+            <th>Entry Number</th>
+            <th>Weight</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.parentEntries.map((entry, index)=>{
+            return (
+              <tr> 
+                <td>{index +1}</td>
+                <td>{entry.weight}</td>
+                <td>{entry.date}</td>
+              </tr>//table rows and table data
+            );
+          })}
+          </tbody>
+      </table>
+     );
+}
+ 
+export default DisplayEntries;
